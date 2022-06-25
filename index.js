0d86947ec2409b035d2096c9f42ea91bef32b035
Jeevan Pant <107680897+tobolt@users.noreply.github.com>
2022-06-26 03:40:02 +0530

    add: setup inquirer (#5)
    * [add] setup inquirer
    
    * [add] setup inquirer

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require('inquirer')
+inquirer
+  .prompt([{
+    type: 'input',
+    name: 'url',
+    message: 'Enter URL'
+  }, {
+    type: 'list',
+    choices: ['Playwright', 'Puppeteer'],
+    name: 'lib',
+    message: 'Choose Library'
+  }]).then((answers) => {
+    console.log(answers)
+  }).catch((error) => {
+    if (error.isTtyError) {
+      console.log('Prompt couldn\'t be rendered in the current environment')
+    } else {
+      console.log('Something went wrong!')
+    }
+  })
