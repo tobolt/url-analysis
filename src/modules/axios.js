f4342db3f2fc3aad7c278aeb8a09e0b5364a3041
Jeevan Pant <jeevan.pant@bolster.ai>
2022-07-04 08:18:15 +0530

    [add] axios as module

---
+++ src/modules/axios.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+async function getSiteData (url) {
+  const request = await axios.get(url);
+  const html = request.data
+
+  return {
+    html
+  }
+}
+
+async function runStatistics (url) {
+  const startJSDOM = new Date()
+  const data = await getSiteData(url)
+  const stopJSDOM = new Date()
+
+  // total words
+  const words = data.html.split(' ').length
+
+  // execution time
+  const executionTime = (stopJSDOM - startJSDOM) / 1000
+
+  return {
+    words,
+    executionTime
+  }
+}
+
+module.exports = { getSiteData, runStatistics }
